f692b784ac7b48a1d6a9db4c639b901c93ada7e7
Tristan Stérin <tristan.sterin@mu.ie>
2021-07-15 18:36:14 +0100

    ok

--- bbsim/tape_scroll.js
+++ bbsim/tape_scroll.js
@@ -1,9 +1,11 @@
+var canvasDiv = document.getElementById("canvasdiv")
 var canvas = document.getElementById("canvas");
 var dragging = false;
 var lastX;
 var tapeOriginX = 5000;
 var marginLeft = -1 * tapeOriginX;
 canvas.style.marginLeft = marginLeft + "px";
+
 
 canvas.addEventListener('mousedown', function (e) {
     var evt = e || event;
@@ -12,7 +14,7 @@ canvas.addEventListener('mousedown', function (e) {
     e.preventDefault();
 }, false);
 
-window.addEventListener('mousemove', function (e) {
+canvas.addEventListener('mousemove', function (e) {
     var evt = e || event;
     if (dragging) {
         var delta = evt.clientX - lastX;
@@ -23,6 +25,10 @@ window.addEventListener('mousemove', function (e) {
     e.preventDefault();
 }, false);
 
-window.addEventListener('mouseup', function () {
+canvas.addEventListener('mouseup', function () {
+    dragging = false;
+}, false);
+
+canvasDiv.addEventListener('mouseleave', function () {
     dragging = false;
 }, false);
